81b4361af9bbfa42cb9f40b8e6941259c7613342
yeojin-c <orlina2001@naver.com>
2026-04-01 13:22:11 +0900

    chore: Docker 및 DB(PostgreSQL) 환경 설정

--- user-service/src/main/java/org/sixpang/userservice/domain/UserRepository.java
+++ user-service/src/main/java/org/sixpang/userservice/domain/UserRepository.java
@@ -1,5 +1,17 @@
 package org.sixpang.userservice.domain;
 
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+//사용자 조회 및 저장 ,중복 체크
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
+
+    Optional<User> findByEmail(String email);
 }
