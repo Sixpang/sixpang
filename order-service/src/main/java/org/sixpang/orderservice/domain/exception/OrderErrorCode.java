db2348082c1b96f21370e50bb1f4365a07b0d3ed
soyeonnan <thduszbzb12@gmail.com>
2026-04-02 21:13:26 +0900

    fix:errorcode변경

---
+++ order-service/src/main/java/org/sixpang/orderservice/domain/exception/OrderErrorCode.java
@@ -0,0 +1,21 @@
+//package org.sixpang.orderservice.domain.exception;
+
+//import lombok.Getter;
+//import lombok.Getter;
+import lombok.Getter;
+import org.sixpang.commonserver.global.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+//@Getter
+//public enum OrderErrorCode {
+
+//    ALREADY_CANCELLED(HttpStatus.BAD_REQUEST);
+//
+//    private final String code;
+//    private final String message;
+//
+//    OrderErrorCode(String code, String message) {
+//        this.code = code;
+//        this.message = message;
+//    }
+//}
---
+++ order-service/src/main/java/org/sixpang/orderservice/domain/model/enums/DeliveryStatus.java
@@ -0,0 +1,14 @@
+package org.sixpang.orderservice.domain.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DeliveryStatus {
+    PENDING("배송 예정"),
+    DELIVERING("배송 중");
+    // 배송 취소?
+
+    private final String description;
+}
