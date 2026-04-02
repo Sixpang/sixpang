e4254282f268181f4ec390e6477accfb09d5c27e
gsemily <gsemily22@gmail.com>
2026-04-02 23:27:59 +0900

    fix: HubRequestDto의 string이 아닌 필드의 조건을 @NotBlank에서 @NotNull로 변경

--- common-server/src/main/java/org/sixpang/commonserver/global/GlobalExceptionHandler.java
+++ common-server/src/main/java/org/sixpang/commonserver/global/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package org.sixpang.commonserver.global;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.sixpang.commonserver.response.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.View;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -22,5 +28,61 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e
+    ){
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        //요청 필드 에러 메시지 리스트
+        List errorMessages = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        //모든 메시지 하나로 합침
+        String errorMessage = String.join(", ", errorMessages);
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                httpStatus,
+                errorMessage
+        );
+
+        return ResponseEntity
+                .status(httpStatus)
+                .body(errorResponseDto);
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodValidationException(HandlerMethodValidationException e){
+        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                httpStatus,
+                "Parameter 값이 잘못되었습니다."
+        );
+
+        return ResponseEntity
+                .status(httpStatus)
+                .body(errorResponseDto);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleUnknownException(Exception e){
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        log.error("[예외 발생] : ", e);
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                httpStatus,
+                e.getMessage()
+        );
+
+        return ResponseEntity
+                .status(httpStatus)
+                .body(errorResponseDto);
     }
 }
--- hub-service/src/main/java/org/sixpang/hubservice/application/dto/HubRequestDto.java
+++ hub-service/src/main/java/org/sixpang/hubservice/application/dto/HubRequestDto.java
@@ -19,18 +19,18 @@ public class HubRequestDto {
     @NotBlank(message = "주소를 입력해주세요.")
     private String address;
 
-    @NotBlank(message = "위도를 입력해주세요.")
+    @NotNull(message = "위도를 입력해주세요.")
     @DecimalMin(value = "-90.0", message = "위도는 -90 미만일 수 없습니다.")
     @DecimalMax(value = "90.0", message = "위도는 90 초과일 수 없습니다.")
     @Digits(integer = 2, fraction = 6, message = "위도는 정수 2자리, 소수 6자리까지 허용됩니다.")
     private BigDecimal latitude;
 
-    @NotBlank(message = "경도를 입력해주세요.")
+    @NotNull(message = "경도를 입력해주세요.")
     @DecimalMin(value = "-180.0", message = "경도는 -180 미만일 수 없습니다.")
     @DecimalMax(value = "180.0", message = "경도는 180 초과일 수 없습니다.")
     @Digits(integer = 3, fraction = 6, message = "경도는 정수 3자리, 소수 6자리까지 허용됩니다.")
     private BigDecimal longitude;
 
-    @NotBlank(message = "허브 운영 상태를 입력해주세요(ACTIVE, SUSPENDED, CLOSED)")
+    @NotNull(message = "허브 운영 상태를 입력해주세요(ACTIVE, SUSPENDED, CLOSED)")
     private HubStatus status;
 }
