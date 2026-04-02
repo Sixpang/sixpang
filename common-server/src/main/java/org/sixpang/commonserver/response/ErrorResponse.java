6d9585755ba6154439297eea2001e650bf3ea208
gsemily <gsemily22@gmail.com>
2026-04-02 22:35:59 +0900

    refactor: ErrorResponse 형식 변경

--- common-server/src/main/java/org/sixpang/commonserver/response/ErrorResponse.java
+++ common-server/src/main/java/org/sixpang/commonserver/response/ErrorResponse.java
@@ -1,23 +1,30 @@
 package org.sixpang.commonserver.response;
 
 import lombok.Getter;
+import org.sixpang.commonserver.global.ErrorCode;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class ErrorResponse {
 
-    private final boolean success;
+    private final int status;
+    private final String errorCode;
     private final String message;
     private final LocalDateTime timestamp;
 
-    public ErrorResponse(String message){
-        this.success = false;
+    public ErrorResponse(ErrorCode errorCode, String message){
+        this.status = errorCode.getStatus().value();
+        this.errorCode = errorCode.getStatus().name();
         this.message = message;
         this.timestamp = LocalDateTime.now();
     }
 
-    public static ErrorResponse of(ErrorCode errorCode){
-        return new ErrorResponse(errorCode.getMessage());
+    public ErrorResponse(HttpStatus httpStatus, String message){
+        this.status = httpStatus.value();
+        this.errorCode = httpStatus.getReasonPhrase();
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
     }
 }
