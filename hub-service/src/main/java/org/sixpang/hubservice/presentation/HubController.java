3ae003727f9f6a1d76a999022ba64c45fc3681de
gsemily <gsemily22@gmail.com>
2026-04-02 17:35:22 +0900

    refaefactor: 인증/인가 구현 전 테스트를 위한 주석 처리 및 default userId 값 삽입

--- hub-service/src/main/java/org/sixpang/hubservice/presentation/HubController.java
+++ hub-service/src/main/java/org/sixpang/hubservice/presentation/HubController.java
@@ -13,6 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -23,16 +24,19 @@ import java.util.UUID;
 public class HubController {
     private final HubService hubService;
 
-    @PreAuthorize("hasRole('MASTER')")
+    // TODO: 인증/인가 구현 완료 후 주석 해제
+    // @PreAuthorize("hasRole('MASTER')")
     @PostMapping("/hubs")
     public ResponseEntity<ApiResponse<HubResponseDto>> register(
+            // TODO: 인증 구현 후 -> AuthenticationPrincipal UUID userId로 적용하기
+            @RequestHeader(value = "X-User-Id", defaultValue = "123e4567-e89b-12d3-a456-426614174000") UUID userId,
             @RequestBody @Valid HubRequestDto requestDto
     ){
-        HubResponseDto responseDto = hubService.register(requestDto);
+        HubResponseDto responseDto = hubService.register(requestDto, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.of("허브가 성공적으로 생성되었습니다.", responseDto));
     }
 
-    @PreAuthorize("hasRole('MASTER')")
+    // @PreAuthorize("hasRole('MASTER')")
     @GetMapping("/hubs")
     public ResponseEntity<ApiResponse<Page<HubResponseDto>>> getHubList(
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
@@ -42,7 +46,6 @@ public class HubController {
         return ResponseEntity.ok(ApiResponse.of("허브 목록이 조회되었습니다.", page));
     }
 
-    @PreAuthorize("hasRole('MASTER')")
     @GetMapping("/hubs/{id}")
     public ResponseEntity<ApiResponse<HubResponseDto>> getHubInfo(
             @PathVariable UUID id
@@ -51,22 +54,25 @@ public class HubController {
         return ResponseEntity.ok(ApiResponse.of("허브 상세 정보가 조회되었습니다.", responseDto));
     }
 
-    @PreAuthorize("hasRole('MASTER')")
+    // @PreAuthorize("hasRole('MASTER')")
     @PatchMapping("/hubs/{id}")
     public ResponseEntity<ApiResponse<HubResponseDto>> updateHubInfo(
             @PathVariable UUID id,
+            // TODO: 인증 인가 후 변경
+            @RequestHeader(value = "X-User-Id", defaultValue = "123e4567-e89b-12d3-a456-426614174000") UUID userId,
             @RequestBody @Valid HubRequestDto requestDto
     ){
-        HubResponseDto responseDto = hubService.updateHubInfo(id, requestDto);
+        HubResponseDto responseDto = hubService.updateHubInfo(id, userId, requestDto);
         return ResponseEntity.ok(ApiResponse.of("허브 정보가 수정되었습니다.", responseDto));
     }
 
-    @PreAuthorize("hasRole('MASTER')")
+    // @PreAuthorize("hasRole('MASTER')")
     @DeleteMapping("/hubs/{id}")
-    public ResponseEntity<ApiResponse<HubResponseDto>> deleteHub(
-            @PathVariable UUID id
+    public ResponseEntity<ApiResponse<Void>> deleteHub(
+            @PathVariable UUID id,
+            @RequestHeader(value = "X-User-Id", defaultValue = "123e4567-e89b-12d3-a456-426614174000") UUID userId
     ){
-        hubService.deleteHub(id);
+        hubService.deleteHub(id, userId);
         return ResponseEntity.ok(ApiResponse.of("허브가 삭제되었습니다.", null));
     }
 }
