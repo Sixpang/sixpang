485d1d8a093a78055fd4400198d5ea8a2ebd30f3
chanhongkwak <rhkrcksghd1@gmail.com>
2026-04-03 15:00:43 +0900

    fix:목록조회 검색기능 추가

--- company-service/src/main/java/org/sixpang/companyservice/domain/repository/CompanyRepository.java
+++ company-service/src/main/java/org/sixpang/companyservice/domain/repository/CompanyRepository.java
@@ -1,5 +1,6 @@
 package org.sixpang.companyservice.domain.repository;
 
+import org.sixpang.companyservice.application.dto.CompanySearchRequest;
 import org.sixpang.companyservice.domain.model.Company;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,4 +14,5 @@ public interface CompanyRepository {
     Company save(Company company);
     Optional<Company> findByIdAndDeletedAtIsNull(UUID companyId);
     Page<Company> findAllByDeletedAtIsNull(Pageable pageable);
+    Page<Company> searchCompanies(CompanySearchRequest request, Pageable pageable);
 }
