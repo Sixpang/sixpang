a960bf43760b638b10090b01a4463438a5ae3aa0
soyeonnan <thduszbzb12@gmail.com>
2026-04-03 14:36:09 +0900

    fix:OrderJpaRepository 수정

--- order-service/src/main/java/org/sixpang/orderservice/infrastructure/repository/OrderJpaRepository.java
+++ order-service/src/main/java/org/sixpang/orderservice/infrastructure/repository/OrderJpaRepository.java
@@ -1,11 +1,16 @@
 package org.sixpang.orderservice.infrastructure.repository;
 
 import org.sixpang.orderservice.domain.model.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderJpaRepository extends JpaRepository<Order, UUID> {
-    // Optional<Order> findByAndDeletedAtIsNull(UUID id);
+     Optional<Order> findByIdAndDeletedAtIsNull(UUID id);
+     Page<Order> findAllByDeletedAtIsNull(Pageable pageable);
+     Page<Order> findBySupplierIdAndDeletedAtIsNull(UUID supplierId, Pageable pageable);
+     Page<Order> findByReceiverIdAndDeletedAtIsNull(UUID receiverId, Pageable pageable);
 }
