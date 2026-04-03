9b043756d4f8b771c40069c2abe7c044849d889b
soyeonnan <thduszbzb12@gmail.com>
2026-04-03 14:31:20 +0900

    fix: OrderRepository 수정

--- order-service/src/main/java/org/sixpang/orderservice/domain/repository/OrderRepository.java
+++ order-service/src/main/java/org/sixpang/orderservice/domain/repository/OrderRepository.java
@@ -1,34 +1,18 @@
 package org.sixpang.orderservice.domain.repository;
 
 import org.sixpang.orderservice.domain.model.entity.Order;
-import org.sixpang.orderservice.domain.model.enums.DeliveryStatus;
-import org.sixpang.orderservice.domain.model.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface OrderRepository extends JpaRepository<Order, UUID>{
+public interface OrderRepository {
 
-    // 주문 단건 조회 (삭제 제외)
+    Order save(Order order);
     Optional<Order> findByIdAndDeletedAtIsNull(UUID id);
-
-    // 공급 업체별 주문 조회 (삭제 제외)
-    Page<Order> findBySupplierIdAndDeletedAtIsNull(UUID supplierId, Pageable pageable);
-
-    // 수령 업체별 주문 조회 (삭제 제외)
-    Page<Order> findByReceiverIdAndDeletedAtIsNull(UUID receiverId, Pageable pageable);
-
-    // 주문 상태별 조회 (삭제 제외)
-    Page<Order> findByOrderStatusAndDeletedAtIsNull(OrderStatus orderStatus, Pageable pageable);
-
-    // 배송 상태별 조회 (삭제 제외)
-    Page<Order> findByDeliveryStatusAndDeletedAtIsNull(DeliveryStatus deliveryStatus, Pageable pageable);
-
-    // 전체 주문 조회 (삭제 제외)
     Page<Order> findAllByDeletedAtIsNull(Pageable pageable);
-
+    Page<Order> findBySupplierIdAndDeletedAtIsNull(UUID supplierId, Pageable pageable);
+    Page<Order> findByReceiverIdAndDeletedAtIsNull(UUID receiverId, Pageable pageable);
 
 }
