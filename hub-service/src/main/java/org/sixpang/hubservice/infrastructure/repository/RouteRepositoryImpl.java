7625f72301eecb8233a5944490dc6b2bff7cd50e
gsemily <gsemily22@gmail.com>
2026-04-06 14:28:53 +0900

    Revert "feat: RouteRepository에 메서드 추가"
    This reverts commit 08ab5428271df2ba8c075183b32e15bd0872287b.

--- hub-service/src/main/java/org/sixpang/hubservice/infrastructure/repository/RouteRepositoryImpl.java
+++ hub-service/src/main/java/org/sixpang/hubservice/infrastructure/repository/RouteRepositoryImpl.java
@@ -1,46 +1,14 @@
 package org.sixpang.hubservice.infrastructure.repository;
 
 import lombok.RequiredArgsConstructor;
-import org.sixpang.hubservice.domain.model.entity.Hub;
 import org.sixpang.hubservice.domain.model.entity.Route;
 import org.sixpang.hubservice.domain.repository.RouteRepository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class RouteRepositoryImpl implements RouteRepository {
     private final RouteRepository routeRepository;
-
-    @Override
-    public Optional<Hub> findByIdAndDeletedAtIsNull(UUID id){
-        return routeRepository.findByIdAndDeletedAtIsNull(id);
-    };
-
-    @Override
-    public Optional<Hub> findByNameAndDeletedAtIsNull(String name){
-        return routeRepository.findByNameAndDeletedAtIsNull(name);
-    };
-
-    @Override
-    public boolean existsByNameAndDeletedAtIsNull(String name){
-        return routeRepository.existsByNameAndDeletedAtIsNull(name);
-    };
-
-    @Override
-    public Page<Hub> findAllByDeletedAtIsNull(Pageable pageable){
-        return routeRepository.findAllByDeletedAtIsNull(pageable);
-    };
-
-    @Override
-    public List<Hub> findAllByDeletedAtIsNull(){
-        return routeRepository.findAllByDeletedAtIsNull();
-    };
 
     @Override
     public Route save(Route route){
