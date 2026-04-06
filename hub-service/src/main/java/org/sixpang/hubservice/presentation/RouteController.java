223a0613329e091b505353dc98f5147c8a17c37a
gsemily <gsemily22@gmail.com>
2026-04-06 19:56:47 +0900

    refactor: uri 수정

--- hub-service/src/main/java/org/sixpang/hubservice/presentation/RouteController.java
+++ hub-service/src/main/java/org/sixpang/hubservice/presentation/RouteController.java
@@ -7,10 +7,7 @@ import org.sixpang.hubservice.application.dto.DirectRouteResponseDto;
 import org.sixpang.hubservice.application.dto.OptimalRouteResponseDto;
 import org.sixpang.hubservice.application.service.RouteService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,27 +18,27 @@ import java.util.UUID;
 public class RouteController {
     private final RouteService routeService;
 
-    @GetMapping("/{departureHubId}/available")
+    @GetMapping("/available")
     public ResponseEntity<ApiResponse<List<AvailableRouteResponseDto>>> getAvailableRoutes(
-            @PathVariable("departureHubId") UUID departureHubId) {
+            @RequestParam("departureHubId") UUID departureHubId) {
 
         List<AvailableRouteResponseDto> responseDto = routeService.getAvailableRoutes(departureHubId);
         return ResponseEntity.ok(ApiResponse.of("허브에서 이동 가능한 경로 목록이 조회되었습니다.", responseDto));
     }
 
-    @GetMapping("/{departureHubId}/{arrivalHubId}/direct")
+    @GetMapping("/direct")
     public ResponseEntity<ApiResponse<DirectRouteResponseDto>> getDirectRoute(
-            @PathVariable("departureHubId") UUID departureHubId,
-            @PathVariable("arrivalHubId") UUID arrivalHubId) {
+            @RequestParam("departureHubId") UUID departureHubId,
+            @RequestParam("arrivalHubId") UUID arrivalHubId) {
 
         DirectRouteResponseDto responseDto = routeService.getDirectRoutes(departureHubId, arrivalHubId);
         return ResponseEntity.ok(ApiResponse.of("두 허브 간 경로가 조회되었습니다.", responseDto));
     }
 
-    @GetMapping("/{departureHubId}/{arrivalHubId}/optimal")
+    @GetMapping("/optimal")
     public ResponseEntity<ApiResponse<OptimalRouteResponseDto>> findOptimalRoute(
-            @PathVariable("departureHubId") UUID departureHubId,
-            @PathVariable("arrivalHubId") UUID arrivalHubId) {
+            @RequestParam("departureHubId") UUID departureHubId,
+            @RequestParam("arrivalHubId") UUID arrivalHubId) {
 
         OptimalRouteResponseDto responseDto = routeService.findOptimalRoute(departureHubId, arrivalHubId);
         return ResponseEntity.ok(ApiResponse.of("최적 이동 경로가 조회되었습니다.", responseDto));
