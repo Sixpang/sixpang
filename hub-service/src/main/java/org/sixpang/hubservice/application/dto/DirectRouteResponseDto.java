2ee8ec3aa513283f56a5c54c0eb12fdd7f743aa4
gsemily <gsemily22@gmail.com>
2026-04-06 17:00:27 +0900

    feat: DirectRouteResponseDto 구현

---
+++ hub-service/src/main/java/org/sixpang/hubservice/application/dto/DirectRouteResponseDto.java
@@ -0,0 +1,25 @@
+package org.sixpang.hubservice.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sixpang.hubservice.domain.model.entity.Route;
+
+import java.math.BigDecimal;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DirectRouteResponseDto {
+    private Long duration;
+    private BigDecimal distance;
+
+    private static DirectRouteResponseDto from(Route route){
+        return DirectRouteResponseDto.builder()
+                .duration(route.getDuration())
+                .distance(route.getDistance())
+                .build();
+    }
+}
