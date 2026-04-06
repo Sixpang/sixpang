874878647253a0af471751ca64e0b2713047d898
gsemily <gsemily22@gmail.com>
2026-04-06 16:01:35 +0900

    refactor: ApiResponse 필드 수정 및 기본 of 메서드 추가

--- common-server/src/main/java/org/sixpang/commonserver/response/ApiResponse.java
+++ common-server/src/main/java/org/sixpang/commonserver/response/ApiResponse.java
@@ -1,26 +1,30 @@
 package org.sixpang.commonserver.response;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Getter
 public class ApiResponse<T> {
 
-    private final boolean success;
+    private final int status;
     private final String message;
     private final T data;
     private final LocalDateTime timestamp;
 
-    public ApiResponse(String message, T data){
-        this.success = true;
+    public ApiResponse(HttpStatus status, String message, T data){
+        this.status = status.value();
         this.message = message;
         this.data = data;
         this.timestamp = LocalDateTime.now();
     }
 
-    public static <T> ApiResponse<T> of(String message, T data){
-        return new ApiResponse<>(message,data);
+    public static <T> ApiResponse<T> of(HttpStatus status, String message, T data){
+        return new ApiResponse<>(status, message, data);
     }
 
+    public static <T> ApiResponse<T> of(String message, T data){
+        return new ApiResponse<>(HttpStatus.OK, message, data);
+    }
 }
