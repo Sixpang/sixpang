30a46dc64366397c34c61f6d8ba05ac2958d37df
gsemily <gsemily22@gmail.com>
2026-04-07 00:07:14 +0900

    refactor: 불필요한 comma 수정

--- hub-service/src/main/java/org/sixpang/hubservice/exception/RouteErrorCode.java
+++ hub-service/src/main/java/org/sixpang/hubservice/exception/RouteErrorCode.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum RouteErrorCode implements ErrorCode {
-    ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브 간 이동 경로를 찾을 수 없습니다."),;
+    ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브 간 이동 경로를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
