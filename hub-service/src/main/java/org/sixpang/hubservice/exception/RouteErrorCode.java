9d754f51d4dcf3a5822b22c2cab48445b28330ed
gsemily <gsemily22@gmail.com>
2026-04-07 00:38:47 +0900

    feat: RouteErrorCode 추가

--- hub-service/src/main/java/org/sixpang/hubservice/exception/RouteErrorCode.java
+++ hub-service/src/main/java/org/sixpang/hubservice/exception/RouteErrorCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum RouteErrorCode implements ErrorCode {
-    ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브 간 이동 경로를 찾을 수 없습니다.");
+    AVAILABLE_ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브에서 갈 수 있는 경로가 없습니다."),
+    DIRECT_ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브 간 이동 경로를 찾을 수 없습니다."),
+    OPTIMAL_ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 허브 간 최적 이동 경로를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
