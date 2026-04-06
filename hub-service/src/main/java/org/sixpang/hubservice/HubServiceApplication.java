589bd703c51fbe5eaef88457d82d2c5e529f7013
gsemily <gsemily22@gmail.com>
2026-04-06 22:02:15 +0900

    feat: sixpang basepackages scan

--- hub-service/src/main/java/org/sixpang/hubservice/HubServiceApplication.java
+++ hub-service/src/main/java/org/sixpang/hubservice/HubServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.sixpang")
 public class HubServiceApplication {
 
     public static void main(String[] args) {
