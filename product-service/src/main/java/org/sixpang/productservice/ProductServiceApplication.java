5262d313b22d8dcdaab3a7af05de7d86cbc012b0
chanhongkwak <rhkrcksghd1@gmail.com>
2026-04-07 04:15:26 +0900

    fix:업체/상품 스키마설정 및 상품 kafka설정 구현

--- product-service/src/main/java/org/sixpang/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/org/sixpang/productservice/ProductServiceApplication.java
@@ -2,7 +2,9 @@ package org.sixpang.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = "org.sixpang")
 public class ProductServiceApplication {
 
