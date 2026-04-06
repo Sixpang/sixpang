5d7010decc2d03a351656eab161281c2f8f7e30d
gsemily <gsemily22@gmail.com>
2026-04-06 14:43:31 +0900

    feat: Route Repository 메서드 추가

--- hub-service/src/main/java/org/sixpang/hubservice/domain/repository/RouteRepository.java
+++ hub-service/src/main/java/org/sixpang/hubservice/domain/repository/RouteRepository.java
@@ -1,7 +1,22 @@
 package org.sixpang.hubservice.domain.repository;
 
 import org.sixpang.hubservice.domain.model.entity.Route;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface RouteRepository {
+    List<Route> findAllByDepartureHubIdAndDeletedAtIsNull(UUID departureHubId);
+
+    Optional<Route> findByDepartureHubIdAndArrivalHubIdAndDeletedAtIsNull(UUID departureHubId, UUID arrivalHubId);
+
+    List<Route> findAllByDeletedAtIsNull();
+
+    void softDeleteRoutesByHubId(UUID hubId, UUID userId);
+
     Route save(Route route);
 }
--- hub-service/src/main/java/org/sixpang/hubservice/infrastructure/repository/RouteRepositoryImpl.java
+++ hub-service/src/main/java/org/sixpang/hubservice/infrastructure/repository/RouteRepositoryImpl.java
@@ -5,13 +5,37 @@ import org.sixpang.hubservice.domain.model.entity.Route;
 import org.sixpang.hubservice.domain.repository.RouteRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 @RequiredArgsConstructor
 public class RouteRepositoryImpl implements RouteRepository {
-    private final RouteRepository routeRepository;
+    private final RouteJpaRepository routeJpaRepository;
+
+    @Override
+    public List<Route> findAllByDepartureHubIdAndDeletedAtIsNull(UUID departureHubId){
+        return routeJpaRepository.findAllByDepartureHubIdAndDeletedAtIsNull(departureHubId);
+    };
+
+    @Override
+    public Optional<Route> findByDepartureHubIdAndArrivalHubIdAndDeletedAtIsNull(UUID departureHubId, UUID arrivalHubId){
+        return routeJpaRepository.findByDepartureHubIdAndArrivalHubIdAndDeletedAtIsNull(departureHubId, arrivalHubId);
+    };
+
+    @Override
+    public List<Route> findAllByDeletedAtIsNull(){
+        return routeJpaRepository.findAllByDeletedAtIsNull();
+    };
+
+    @Override
+    public void softDeleteRoutesByHubId(UUID hubId, UUID userId){
+        routeJpaRepository.softDeleteRoutesByHubId(hubId, userId);
+    };
 
     @Override
     public Route save(Route route){
-       return routeRepository.save(route);
+        return routeJpaRepository.save(route);
     };
 }
